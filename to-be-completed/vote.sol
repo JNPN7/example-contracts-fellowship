682dffe6a0944f621736859aeac2122918f7f146
JNPN7 <jpphanju54@gmail.com>
2022-09-22 20:49:05 +0545

    vote

--- to-be-completed/vote.sol
+++ to-be-completed/vote.sol
@@ -9,32 +9,42 @@ contract cityPoll {
     }
 
 
-    mapping() public cities; //mapping city Id with the City ctruct - cityId should be uint256
-    mapping() hasVoted; //mapping to check if the address/account has voted or not
+    mapping(uint256 => City) public cities; //mapping city Id with the City ctruct - cityId should be uint256
+    mapping(address => bool) hasVoted; //mapping to check if the address/account has voted or not
 
     address owner;
     uint256 public cityCount = 0; // number of city added
     constructor() public {
-    
+        owner = msg.sender;
+        cities[0].cityName = "ktm";
+        cities[0].vote = 0;
+        cities[1].cityName = "bkt";
+        cities[1].vote = 0;
+        cityCount = 2;
     //TODO set contract caller as owner
     //TODO set some intitial cities.
     }
  
  
-    function addCity() public {
+    function addCity(string memory city_name) public {
       //  TODO: add city to the CityStruct
+      require(msg.sender == owner, "You are not owner of this contract");
 
+      cities[cityCount].cityName = city_name;
+      cities[cityCount].vote = 0;
+      cityCount ++;      
     }
     
-    function vote() public {
-        
+    function vote(uint256 city) public {
         //TODO Vote the selected city through cityID
-
+        cities[city].vote += 1;
     }
-    function getCity() public view returns (string memory) {
-     // TODO get the city details through cityID
+    function getCity(uint256 city) public view returns (string memory) {
+        // TODO get the city details through cityID
+        return (cities[city].cityName);
     }
-    function getVote() public view returns (uint256) {
-    // TODO get the vote of the city with its ID
+    function getVote(uint256 city) public view returns (uint256) {
+        // TODO get the vote of the city with its ID
+        return cities[city].vote;
     }
 }
